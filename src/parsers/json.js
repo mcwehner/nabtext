a822773efb7761d970703360894b4162cf892c71
Michael Wehner <mcwehner@gmail.com>
2011-04-25 02:36:57 -0700

    Commit of stale changes (bad, bad, bad...)

---
+++ src/parsers/json.js
@@ -0,0 +1,8 @@
+/*  JSON "parser" for nabtext.
+    
+    TODO: Well... everything. Some thoughts:
+    
+    * Add a callback hook into the overall loading/parsing methods, allowing
+      for a custom callback to be run after the loaded JSON (or .mo's, .po's,
+      etc.) but before it's actually added to the string dictionary.
+*/
